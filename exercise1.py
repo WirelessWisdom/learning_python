bb290dda2fd67657c5738c626d80768c90cefe44
Nina_Kazim <ninakazimirskaya@gmail.com>
2023-12-09 12:09:17 +0100

    fixed tasks with help of the best teacher ever

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,5 @@
 # task 1
-import datetime
-import this  # appeared to be some pseudo philosophy by some dinosaur developer
+import this  # appeared to be some philosophy by some dinosaur developer
 
 # task 2
 from math import sqrt
@@ -16,35 +15,41 @@ print(y)
 
 # task 3
 name = input("Please enter your name: ")
-print("Hello " + name)
+#print("Hello " + name)
+print("Hello {}".format(name))
+print(f"Hello {name}")
+
 
 # task 4
 exchange_rate = 36.5
-amount = input("Please enter the amount of UAH you want to exchange: ")
-uah = int(amount)
-usd = round(uah / exchange_rate, 2)
-usd_to_print = str(usd)
-print("You will receive " + usd_to_print + " USD")
+amount = int(input("Please enter the amount of UAH you want to exchange: "))
+#uah = int(amount)
+usd = round(amount / exchange_rate, 2)
+#usd_to_print = str(usd)
+print(f"You will receive {usd} USD")
 
 # task 5 aka Zavdannya 2
 snake_case = input("Please enter 3 words in format a_b_c: ")
 splited = snake_case.split("_")
 
+capitalized_words = []
 for word in splited:
     cap_word = str(word.capitalize())
-    print(cap_word)
+    capitalized_words.append(cap_word)
+print("".join(capitalized_words))
+
+#for word in splited:
+#    cap_word = str(word.capitalize())
+#    print(cap_word)
+
 
 # task 6 aka Zavdannya 3
 person = input("Please enter name and dates of birth and death, use * as separator: ")
 l = person.split("*")
 name = l[0]
-date_of_birth = l[1].split("-")
-date_of_death = l[2].split("-")
+date_of_birth = int(l[1].split("-")[0])
+date_of_death = int(l[2].split("-")[0])
 
-year_of_birth = int(date_of_birth[0])
-year_of_death = int(date_of_death[0])
-
-age = str(year_of_death - year_of_birth)
-print(name)
-print("Died in age of " + age)
+age = date_of_death - date_of_birth
+print(f"{name}\nDied in age of {age}")
 
