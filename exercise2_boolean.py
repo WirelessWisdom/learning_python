18da0acf289af782b56638983c205c9af904ac78
Nina_Kazim <ninakazimirskaya@gmail.com>
2023-12-09 14:43:35 +0100

    fixed tasks with help of the best teacher ever

---
+++ exercise2_boolean.py
@@ -0,0 +1,13 @@
+age = int(input("Please enter your age: "))
+if age < 2:
+    print("You are a baby")
+elif 2 <= age < 4:
+    print("You are a kid")
+elif 4 <= age < 13:
+    print("You are a child")
+elif 13 <= age < 20:
+    print("You are a teenager")
+elif 20 <= age < 65:
+    print("You are a gown-up")
+else:
+    print("You are a senior")
