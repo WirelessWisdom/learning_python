9db933dc6176c6073a1c94cf0fe9f5e52ba48c81
Nina_Kazim <ninakazimirskaya@gmail.com>
2023-12-09 20:30:23 +0100

    finished 2 task

--- exercise2_boolean.py
+++ exercise2_boolean.py
@@ -1,5 +1,5 @@
+# task 1
 age = int(input("Please enter your age: "))
-
 if age < 2:
     print("You are a baby")
 elif 2 <= age < 4:
@@ -12,3 +12,15 @@ elif 20 <= age < 65:
     print("You are a gown-up")
 else:
     print("You are a senior")
+
+# task 2 - move zeros to the end of the list
+input_numbers = input("Please enter a few numbers, use coma symbol as separator: ")
+list_of_numbers = input_numbers.split(",")
+
+for number in list_of_numbers:
+    if number == "0":
+        list_of_numbers.remove(number)
+        list_of_numbers.append(number)
+
+print(list_of_numbers)
+
